8b06987147bf2e66900c6defc9797ca4290f85a7
Aryan-Izhar <aryanizhar2003@gmail.com>
2025-12-10 15:01:17 +0500

    Add social media links to Layout component

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -253,6 +253,7 @@ export const Layout: React.FC<LayoutProps> = ({ children, user, onLogout, notifi
                 Your one-stop destination for expert digital services. High-quality work, delivered on time.
               </p>
               <div className="flex gap-4">
+                
                 <a href="https://www.facebook.com/share/1FXQ7o2avS/" className="w-8 h-8 rounded-full bg-gray-800 flex items-center justify-center hover:bg-premium-royal transition-colors"><Facebook className="w-4 h-4" /></a>
                 <a href="https://www.instagram.com/techr_each?igsh=MmpveHZ2aHhwcGo3" className="w-8 h-8 rounded-full bg-gray-800 flex items-center justify-center hover:bg-premium-royal transition-colors"><Instagram className="w-4 h-4" /></a>
                 <a href="https://whatsapp.com/channel/0029Vb75DdLCXC3N2LEdye1O"className="w-8 h-8 rounded-full bg-gray-800 flex items-center justify-center hover:bg-green-500 transition-colors"><Whatsapp className="w-4 h-4 text-white" /> </a>
